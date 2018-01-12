9aa7d4ae3031398e7ec0e613a12deccf464e6eb4
williamjohngardner <bill@williamjohngardner.com>
2018-01-12 14:03:47 -0500

    refactored as a class

--- provider.py
+++ provider.py
@@ -4,67 +4,81 @@ import tweepy
 import os
 import config
 
+class SocialMediaPerformance:
 
-def facebook_api():
+    def __init__(self):
+        self.twitter_consumer_key = config.twitter_consumer_key
+        self.twitter_consumer_secret = config.twitter_consumer_secret
+        self.twitter_access_token = config.twitter_access_token
+        self.twitter_access_token_secret = config.twitter_access_token_secret
 
-    fb_user_access_token = config.fb_user_access_token
+    def facebook_api(self):
 
-    graph = facebook.GraphAPI(access_token= fb_user_access_token)
+        fb_user_access_token = config.fb_user_access_token
 
-    events = graph.request('/search?q=Poetry&type=event&limit=10000')
+        graph = facebook.GraphAPI(access_token= fb_user_access_token)
 
-    eventList = events['data']
+        events = graph.request('/search?q=Poetry&type=event&limit=10000')
 
-    eventid = eventList[1]['id']
+        eventList = events['data']
 
-    event1 = graph.get_object(id=eventid,
-     fields='attending_count,can_guests_invite,category,cover,declined_count,description,end_time,guest_list_enabled,interested_count,is_canceled,is_page_owned,is_viewer_admin,maybe_count,noreply_count,owner,parent_group,place,ticket_uri,timezone,type,updated_time')
-    attenderscount = event1['attending_count']
-    declinerscount = event1['declined_count']
-    interestedcount = event1['interested_count']
-    maybecount = event1['maybe_count']
-    noreplycount = event1['noreply_count']
+        eventid = eventList[1]['id']
 
-    attenders = requests.get("https://graph.facebook.com/v2.7/"+eventid+"/attending?access_token="+fb_user_access_token+"&limit="+str(attenderscount))
-    attenders_json = attenders.json()
+        event1 = graph.get_object(id=eventid,
+         fields='attending_count,can_guests_invite,category,cover,declined_count,description,end_time,guest_list_enabled,interested_count,is_canceled,is_page_owned,is_viewer_admin,maybe_count,noreply_count,owner,parent_group,place,ticket_uri,timezone,type,updated_time')
+        attenderscount = event1['attending_count']
+        declinerscount = event1['declined_count']
+        interestedcount = event1['interested_count']
+        maybecount = event1['maybe_count']
+        noreplycount = event1['noreply_count']
 
-    print(attenders_json)
+        attenders = requests.get("https://graph.facebook.com/v2.7/"+eventid+"/attending?access_token="+fb_user_access_token+"&limit="+str(attenderscount))
+        attenders_json = attenders.json()
 
-
-def twitter_api():
-
-    twitter_consumer_key = config.twitter_consumer_key
-    twitter_consumer_secret = config.twitter_consumer_secret
-    twitter_access_token = config.twitter_access_token
-    twitter_access_token_secret = config.twitter_access_token_secret
-
-    auth = tweepy.OAuthHandler(twitter_consumer_key, twitter_consumer_secret)
-    auth.set_access_token(twitter_access_token, twitter_access_token_secret)
-
-    api = tweepy.API(auth)
-
-    user = api.get_user('williamjgardner')
-    retweets = api.retweets_of_me(count=1)
-    status = api.get_status('888376794780401664')
-
-    print('Data for User: ' + user.screen_name)
-    print('Total Follower Count: ' + str(user.followers_count))
-    print(status.text)
-    # for retweet in retweets:
-    #     print(retweet)
+        print(attenders_json)
 
 
-def instagram_api():
+    def twitter_api(self):
 
-    from InstagramAPI import InstagramAPI
+        auth = tweepy.OAuthHandler(self.twitter_consumer_key, self.twitter_consumer_secret)
+        auth.set_access_token(self.twitter_access_token, self.twitter_access_token_secret)
 
-    instagram_username= config.instagram_username
-    instagram_password = config.instagram_password
-    InstagramAPI = InstagramAPI.InstagramAPI(instagram_username, instagram_password)
-    InstagramAPI.login()
+        api = tweepy.API(auth)
+
+        user = api.get_user('williamjgardner')
+        retweets = api.retweets_of_me(count=1)
+        status = api.get_status('888376794780401664')
+
+        print('Data for User: ' + user.screen_name)
+        print('Total Follower Count: ' + str(user.followers_count))
+        print(status.text)
+        # for retweet in retweets:
+        #     print(retweet)
 
 
+    def instagram_api(self):
 
-# twit = twitter_api()
-# face = facebook_api()
-insta = instagram_api()
+        from InstagramAPI import InstagramAPI
+
+        instagram_username= config.instagram_username
+        instagram_password = config.instagram_password
+
+        api = InstagramAPI(instagram_username, instagram_password)
+        api.login() # login
+        api.tagFeed("cat") # get media list by tag #cat
+        media_id = api.LastJson # last response JSON
+        api.like(media_id["ranked_items"][0]["pk"]) # like first media
+        api.getUserFollowers(media_id["ranked_items"][0]["user"]["pk"]) # get first media owner followers
+
+    def document_output(self):
+
+        total_fans = twitter_api.retweets
+        # tab_doc = {}'\t'{}'\t'{}'\t'{}'\n'.format()
+        print('Total Fans: ' + str(total_fans))
+
+
+social = SocialMediaPerformance()
+social.twitter_api()
+# social.facebook_api()
+social.instagram_api()
+# social.document_output()
